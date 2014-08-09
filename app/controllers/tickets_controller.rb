9e86ce8ec2e2d021599d32a983ad94701304f9dc
Frederic-Barcelone <ftetard@mobilesdynamics.com>
2014-08-09 14:52:08 +0200

    We can now edit tickets.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,8 +7,12 @@ class TicketsController < ApplicationController
 		@ticket = @project.tickets.build
 	end
 
+	def edit
+	end
+	
 	def show
 	end
+
 
 	def create
 		@ticket = @project.tickets.build(ticket_params)
@@ -20,6 +24,18 @@ class TicketsController < ApplicationController
 			render "new"
 		end
 	end
+
+
+	def update
+		if @ticket.update(ticket_params)
+			flash[:notice] = "Ticket has been updated."
+			redirect_to [@project, @ticket]
+		else
+			flash[:alert] = "Ticket has not been updated."
+			render action: "edit"
+		end
+	end
+
 
 	private 
 
